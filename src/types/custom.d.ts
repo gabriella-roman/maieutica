69c0f71f55874bd0b5831f76f998893b98240d66
Gabriella Roman <gabriellaperniconi@gmail.com>
2025-03-31 11:57:27 -0300

    Remove Bootstrap dependency and related type declarations

--- src/types/custom.d.ts
+++
@@ -1,8 +0,0 @@
-// src/types/custom.d.ts
-declare global {
-  interface Window {
-    bootstrap: typeof import('bootstrap');  // Importa os tipos do Bootstrap diretamente
-  }
-}
-
-export {};  // Isso ajuda a garantir que o arquivo seja tratado como um módulo
