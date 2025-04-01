14b1a341ceb85ab3adfd7bed291a389c2b50c05a
Gabriella Roman <gabriellaperniconi@gmail.com>
2025-04-01 17:38:40 -0300

    Enhance Footer component with responsive design and add social media icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,70 +2,82 @@ import { ColoredLine } from '../../components/ColoredLine/ColoredLine'
 import styles from './Footer.module.css'
 import logo from '../../assets/images/logo-maieutica.svg'
 import { NavbarFooter } from '../../components/NavbarFooter/NavbarFooter'
+import instagram from '../../assets/icons/instagram.svg'
+import facebook from '../../assets/icons/facebook.svg'
+import linkedin from '../../assets/icons/linkedin.svg'
 
 export function Footer() {
   return (
-    <div className={`${styles.footer} w-100`}>
+    <div className={styles.footer}>
       <ColoredLine />
 
-      <div className={`${styles.container} container-fluid`}>
-        <div className="row">
-          <div className="col-12 col-md-3">
-            <img src={logo} alt="Logo" height={200} />
-          </div>
+      <div className={styles.container}>
+        <div>
+          <img src={logo} alt="Logo" height={200} />
+        </div>
 
-          <div className="col-12 col-md-3">
-            <NavbarFooter
-              title="Serviços"
-              options={[
-                { title: "Processo seletivo", navigate: "/" },
-                { title: "Perfil psicológico", navigate: "/" },
-                { title: "Aconselhamento de carreira", navigate: "/" },
-                { title: "Outplacement", navigate: "/" },
-                { title: "Avaliação do idioma inglês", navigate: "/" }
-              ]}
-            />
-          </div>
+        <div className={styles.box}>
+          <NavbarFooter
+            title="Serviços"
+            options={[
+              { title: "Processo seletivo", navigate: "/" },
+              { title: "Perfil psicológico", navigate: "/" },
+              { title: "Aconselhamento de carreira", navigate: "/" },
+              { title: "Outplacement", navigate: "/" },
+              { title: "Avaliação do idioma inglês", navigate: "/" }
+            ]}
+          />
+        </div>
 
-          <div className="col-12 col-md-3">
-            <NavbarFooter
-              title="Maiêutica RH Educacional"
-              options={[
-                { title: "Ver vagas", navigate: "/" },
-                { title: "Fale conosco", navigate: "/contact-us" },
-                { title: "Dúvidas frequentes", navigate: "/" },
-                { title: "Sobre nós", navigate: "/about-us" }
-              ]}
-            />
-          </div>
+        <div className={styles.box}>
+          <NavbarFooter
+            title="Maiêutica RH Educacional"
+            options={[
+              { title: "Ver vagas", navigate: "/" },
+              { title: "Fale conosco", navigate: "/contact-us" },
+              { title: "Dúvidas frequentes", navigate: "/" },
+              { title: "Sobre nós", navigate: "/about-us" }
+            ]}
+          />
+        </div>
 
-          <div className="col-12 col-md-3">
-            <div style={{ marginTop: 8 }}>
-              <div>
-                <h1 className={styles.h1}>E-mail</h1>
-                <p className={styles.p}>contato@maieuticarh.com.br</p>
-              </div>
+        <div>
+          <h1 className={styles.title}>
+            Contato
+          </h1>
+          <div className={styles.box}>
+            <div>
+              <h1 className={styles.h1}>E-mail</h1>
+              <p className={styles.p}>contato@maieuticarh.com.br</p>
+            </div>
 
-              <div>
-                <h1 className={styles.h1}>Localização</h1>
-                <p className={styles.p}>São Paulo - SP</p>
-              </div>
+            <div>
+              <h1 className={styles.h1}>Localização</h1>
+              <p className={styles.p}>São Paulo - SP</p>
+            </div>
 
-              <div>
-                <h1 className={styles.h1}>CNPJ</h1>
-                <p className={styles.p}>32.175.487/0001-30</p>
-              </div>
+            <div>
+              <h1 className={styles.h1}>CNPJ</h1>
+              <p className={styles.p}>32.175.487/0001-30</p>
+            </div>
+
+            <div className={styles.icons}>
+              <img src={instagram} alt="Instagram" />
+
+              <img src={facebook} alt="Facebook" />
+
+              <img src={linkedin} alt="Linkedin" />
             </div>
           </div>
         </div>
       </div>
 
-      <div className={`${styles.bottom} text-center py-3`}>
-        <p>
+      <div className={styles.bottom}>
+        <p className={styles.p}>
           Desenvolvido por Soav Tech © Todos os direitos reservados.
         </p>
 
-        <p>
+        <p className={styles.p}>
           Soav Tech
         </p>
       </div>
